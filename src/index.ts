addadb2c6a2d420586c8d6e9eb04865da751804f
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2024-05-09 11:57:56 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response, urlencoded } from "express";
 import { sequelize } from "./Database/database";
-
-console.log("Hello World");
 
 const app = express();
 const port = 6000;
 
 app.use(express.json());
-
-app.route("/").get((res: Response, req: Request) =>{
-    res.send("Hello World");
+app.use(urlencoded({extended: true}));
+const validate = (req: Request, res: Response, next: NextFunction) => {
+    console.log("HOLA");
+    return next();
+};
+app.get("/",validate,(req: Request, res: Response) =>{
+    res.send(req.headers.authorization);
 });
 
 app.listen(port,() =>{
@@ -17,3 +19,17 @@ app.listen(port,() =>{
 });
 
 sequelize.sync();
+
+
+//JWT testing area
+/*
+
+/auth/register => userAuth. jwt.sing Return token
+
+cuando se creo
+cuando expira
+
+Registrar persona
+
+logear
+*/
